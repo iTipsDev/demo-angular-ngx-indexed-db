a11d89b7bcd996a58bc85f329ab31d82d0bf0947
Bruce V Wilcox <brucevwilcox@itips.info>
2023-10-23 11:21:43 -0600

    2023-10-23T11:21

--- c.sh
+++ c.sh
@@ -3,7 +3,7 @@ DLIN="==========================================================================
 CDATE=$(date '+%Y-%m-%dT%H:%M')
 
 echo $LINE
-echo "git pull origin main"
+echo "git pull origin master"
 echo $LINE
 git pull origin main
 echo $DLIN
@@ -31,7 +31,7 @@ git status
 echo $DLIN
 
 echo $LINE
-echo "git push origin main"
+echo "git push origin master"
 echo $LINE
 git push origin main
 echo $DLIN
